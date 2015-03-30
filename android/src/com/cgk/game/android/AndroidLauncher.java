3c43a6be18930559256acef8fe5525e35d4c437c
iplukens <ian@botch.com>
2015-03-29 22:11:14 -0400

    readded disable compass and accelerometer

--- android/src/com/cgk/game/android/AndroidLauncher.java
+++ android/src/com/cgk/game/android/AndroidLauncher.java
@@ -8,9 +8,11 @@ import com.cgk.game.CardGameKawaii;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
 		initialize(new CardGameKawaii(), config);
 	}
 }
